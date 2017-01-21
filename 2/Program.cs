3830a6855ec85fe92b836eaafff0fdf302c721f2
VMentore <veselin.st.stanchev@gmail.com>
2017-01-21 23:17:35 +0200

    new

---
+++ 2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double r = double.Parse(Console.ReadLine());
+            double a =r * r * Math.PI;
+
+
+            Console.WriteLine("{0:F12}", a);
+        }
+    }
+}
---
+++ 3/3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal sum = 0;
+            int a = int.Parse(Console.ReadLine());
+           
+                for (int i = 0; i <a; i++)
+                {
+                    decimal n = decimal.Parse(Console.ReadLine());
+                    sum += n;
+                }
+         
+           
+            Console.WriteLine(sum);
+
+        }
+    }
+}
